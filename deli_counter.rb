901d953f1bf72156ad4f5a168e19a381478f9a0c
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-06 20:36:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,7 @@ def take_a_number(array, name)
   
   def now_serving(array)
     if array.length >= 1 
-      puts "Now serving #{array[0]}."
+      puts "Currently serving #{array[0]}."
       array.shift
     else puts "There is nobody waiting to be served!"
   end
